ad8ab7492e403709ecf450d760607ca1a28eacc4
Kevin Winter <kevin.winter@edu.bc.fi>
2022-06-02 23:09:46 +0300

    CRUD operations complete

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -7,19 +7,25 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Entity\Post;
+use App\Form\PostFormType;
 use App\Repository\PostRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class PostsController extends AbstractController
 {
+    private $em;
     private $postRepository;
-    public function __construct(PostRepository $postRepository)
+    public function __construct(PostRepository $postRepository, EntityManagerInterface $em)
     {
         $this->postRepository = $postRepository;
+        $this->em = $em;
     }
 
-    #[Route('/posts', name: 'app_posts')]
+
+    #[Route('/', name: 'app_posts')]
     public function index(): Response
     {
         $posts = $this->postRepository->findAll();
@@ -29,7 +35,108 @@ class PostsController extends AbstractController
         ]);
     }
 
-    #[Route('/posts/{id}', methods: ['GET'], name: 'posts')]
+
+    #[Route('/posts/create', name: 'create_post')]
+    public function create(Request $request): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostFormType::class, $post);
+
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+            $newPost = $form->getData();
+
+            $imagePath = $form->get('imagePath')->getData();
+            if ($imagePath) {
+                $newFileName = uniqid() . '.' . $imagePath->guessExtension();
+
+                try {
+                    $imagePath->move(
+                        $this->getParameter('kernel.project_dir') . '/public/uploads', $newFileName
+                    );
+                } catch (FileException $e) {
+                    return new Response($e->getMessage());
+                }
+
+                $newPost->setImagePath('/uploads/' . $newFileName);
+            }
+
+            $this->em->persist($newPost);
+            $this->em->flush();
+
+            return $this->redirectToRoute('app_posts');
+        }
+
+        return $this->render('posts/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    #[Route('/posts/edit/{id}', name: 'edit_post')]
+    public function edit($id, Request $request): Response
+    {
+        $post = $this->postRepository->find($id);
+        $form = $this->createForm(PostFormType::class, $post);
+
+        $form->handleRequest($request);
+        $imagePath = $form->get('imagePath')->getData();
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($imagePath) {
+                if ($post->getImagePath() !== null) {
+                    if (file_exists(
+                        $this->getParameter('kernel.project_dir') . $post->getImagePath()
+                        )) {
+                            $this->getParameter('kernel.project_dir') . $post->getImagePath();
+                    }
+                    $newFileName = uniqid() . '.' . $imagePath->guessExtension();
+
+                    try {
+                        $imagePath->move(
+                            $this->getParameter('kernel.project_dir') . '/public/uploads',
+                            $newFileName
+                        );
+                    } catch (FileException $e) {
+                        return new Response($e->getMessage());
+                    }
+
+                    $post->setImagePath('/uploads/' . $newFileName);
+                    $this->em->flush();
+
+                    return $this->redirectToRoute('app_posts');
+                }
+            } else {
+                $post->setTitle($form->get('title')->getData());
+                $post->setAuthor($form->get('author')->getData());
+                $post->setSubject($form->get('subject')->getData());
+                $post->setBody($form->get('body')->getData());
+
+                $this->em->flush();
+                return $this->redirectToRoute('app_posts');
+            }
+        }
+
+        return $this->render('posts/edit.html.twig', [
+            'post' => $post,
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    #[Route('/posts/delete/{id}', methods: ['GET', 'DELETE'], name: 'delete_post')]
+    public function delete($id): Response
+    {
+        $post = $this->postRepository->find($id);
+        $this->em->remove($post);
+        $this->em->flush();
+
+        return $this->redirectToRoute('app_posts');
+    }
+
+
+    #[Route('/posts/{id}', methods: ['GET'], name: 'view_post')]
     public function view($id): Response
     {
         $post = $this->postRepository->find($id);
---
+++ src/Form/PostFormType.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+// use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class PostFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'attr' => array(
+                    'class' => 'bg-transparent block border-b-2 w-full h-10 text-2xl outline-none mb-7',
+                    'placeholder' => 'Enter a title for the blog post'
+                )
+            ])
+
+            ->add('author', TextType::class, [
+                'attr' => array(
+                    'class' => 'bg-transparent block border-b-2 w-full h-10 text-2xl outline-none mb-7',
+                    'placeholder' => 'Enter name of author'
+                )
+            ])
+
+            ->add('subject', TextType::class, [
+                'attr' => array(
+                    'class' => 'bg-transparent block border-b-2 w-full h-10 text-2xl outline-none mb-7',
+                    'placeholder' => 'Enter an optional subject heading'
+                )
+            ])
+
+            // ->add('date')
+
+            ->add('body', TextareaType::class, [
+                'attr' => array(
+                    'class' => 'bg-transparent block border-b-2 w-full h-60 text-2xl outline-none mb-7',
+                    'placeholder' => 'Enter the main body text of the blog post'
+                )
+            ])
+
+            // ->add('imagePath', FileType::class, [
+            //     'attr' => array(
+            //         'class' => 'py-7'
+            //         // 'placeholder' => 'Enter the main body text of the blog post'
+            //     ),
+            //     'label' => false
+            // ])
+            ->add('imagePath', FileType::class, array(
+                'required' => false,
+                'mapped' => false,
+                'attr' => array(
+                    'class' => 'py-2 block'
+                )
+            ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
